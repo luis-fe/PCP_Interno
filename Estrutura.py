bf2e6fb216a98a32a2ec1f16bf82a886edc36370
luis-fe <luis_engp@hotmail.com>
2023-07-19 15:01:50 -0300

    tela login

--- Estrutura.py
+++ Estrutura.py
@@ -43,7 +43,7 @@ def EstruturaFiltroEngenharia(colecoes, Engenharia):
                             "cv.quantidade "  
                             " FROM tcp.CompVarSorGraTam cv "
                             "JOIN tcp.DadosGeraisEng d ON cv.codempresa = d.codEmpresa AND cv.codProduto = d.codEngenharia " 
-                            " WHERE cv.codEmpresa = 1 AND d.codColecao in ("+ colecoes+") and codProduto in ("+Engenharia+")", conn)
+                            " WHERE cv.codEmpresa = 1 AND d.codColecao in ("+ colecoes+") and cv.codProduto in ('"+Engenharia+"')", conn)
     estrutura.rename(
         columns={'tipo': '01- tipo', "codColecao": '02- codColecao','codProduto':'03- codProduto'
                  ,'codSortimento':'04- codSortimento','tamanho':'05- tamanho','corProduto':'06- corProduto'
