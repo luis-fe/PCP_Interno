b30118a712a39b6ed9fe21ab69111f2db9408ce8
luis-fe <luis_engp@hotmail.com>
2023-07-25 09:24:54 -0300

    Commit Plano

--- Plano.py
+++ Plano.py
@@ -7,6 +7,6 @@ def ObeterPlanos():
     planos = pd.read_sql('SELECT * FROM pcp."Plano" '
                          ' ORDER BY codigo ASC;',conn)
     planos.rename(
-        columns={'codigo': '01- Codigo Plano'},
+        columns={'codigo': '01- Codigo Plano', 'descricao do Plano': '02- Descricao do Plano', 'incioVenda': '03- Inicio Venda'},
         inplace=True)
     return planos
