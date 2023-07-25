1694873e976663916ded2a41327be6ac1e72b592
luis-fe <luis_engp@hotmail.com>
2023-07-25 09:23:29 -0300

    Commit Plano

--- Plano.py
+++ Plano.py
@@ -6,4 +6,7 @@ def ObeterPlanos():
     conn = ConexaoPostgreMPL.conexao()
     planos = pd.read_sql('SELECT * FROM pcp."Plano" '
                          ' ORDER BY codigo ASC;',conn)
+    planos.rename(
+        columns={'codigo': '01- Codigo Plano'},
+        inplace=True)
     return planos
