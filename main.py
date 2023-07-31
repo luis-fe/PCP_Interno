c1faa2de3e90b3aa6a332fddae4604786c49508a
luis-fe <luis_engp@hotmail.com>
2023-07-31 08:08:51 -0300

    Commit Plano

--- main.py
+++ main.py
@@ -84,7 +84,7 @@ def criar_usuario():
     nome = novo_usuario.get('nome')
     senha = novo_usuario.get('senha')
     # inserir o novo usuário no banco de dados
-    c = Usuarios.ObterUsuariosCodigo(codigo)
+    c, x, y = Usuarios.ObterUsuariosCodigo(codigo)
     if c != 0:
         return jsonify({'message': f'Novo usuário:{codigo}- {nome} ja existe'}), 200
     else:
