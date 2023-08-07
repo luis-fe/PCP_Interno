80a048928f57a94deb4099ce32e21a97fcce787a
luis-fe <luis_engp@hotmail.com>
2023-08-07 14:41:48 -0300

    Commit Plano

--- Vendas.py
+++ Vendas.py
@@ -58,7 +58,7 @@ def VendasporSku(plano , aprovado= True, excel = False):
             'qtdePedida': 'sum'})
         print('excel True')
         Pedido['reduzido'] = Pedido['reduzido'].astype(str)
-        Pedido.sort_values(by='reduzido', inplace=True)
+        Pedido.sort_values(by='qtdePedida', inplace=True)
 
         return Pedido
 
