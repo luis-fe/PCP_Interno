2f153921a5215c6c439da2abbf9d9270c9ea7cc8
luis-fe <luis_engp@hotmail.com>
2023-08-09 11:27:35 -0300

    Commit Plano

--- Vendas.py
+++ Vendas.py
@@ -141,6 +141,7 @@ def VendasporSku(client_ip,plano , aprovado= True, excel = False):
             Pedido['categoria'] = Pedido.apply(lambda row: Categoria('BATA', row['descricao'],'CAMISA',row['categoria']  ), axis=1)
             Pedido['categoria'] = Pedido.apply(lambda row: Categoria('MEIA', row['descricao'],'MEIA',row['categoria']  ), axis=1)
             Pedido['categoria'] = Pedido.apply(lambda row: Categoria('CUECA', row['descricao'],'CUECA',row['categoria']  ), axis=1)
+            Pedido['ABC%Categ'] = Pedido.groupby(['MARCA','categoria'])['engenharia'].cumcount() + 1
 
 
 
