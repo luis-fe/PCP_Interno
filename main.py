9c442da9f4b702289b134ee24c0efa029d3ccf61
luis-fe <luis_engp@hotmail.com>
2023-08-15 10:16:11 -0300

    Commit Plano

--- main.py
+++ main.py
@@ -48,65 +48,7 @@ def TelaUsuarios():
     return render_template('TelaUsuarios.html')
 
 
-@app.route('/pcp/api/Usuarios', methods=['PUT'])
-@token_required
-def criar_usuario():
-    # Obtenha os dados do corpo da requisição
-    novo_usuario = request.get_json()
-    # Extraia os valores dos campos do novo usuário
-    codigo = novo_usuario.get('codigo')
-    nome = novo_usuario.get('nome')
-    senha = novo_usuario.get('senha')
-    # inserir o novo usuário no banco de dados
-    c, x, y = Usuarios.ObterUsuariosCodigo(codigo)
-    if c != 0:
-        return jsonify({'message': f'Novo usuário:{codigo}- {nome} ja existe'}), 200
-    else:
-        Usuarios.InserirUsuario(codigo, nome, senha)
-        # Retorne uma resposta indicando o sucesso da operação
-        return jsonify({'message': f'Novo usuário:{codigo}- {nome} criado com sucesso'}), 200
 
-@app.route('/pcp/api/Usuarios/<string:codigoUsuario>', methods=['DELETE'])
-@token_required
-def delet_Usuario(codigoUsuario):
-    # Obtém os dados do corpo da requisição (JSON)
-    data = request.get_json()
-    codigoUsuario = str(codigoUsuario)
-    # Verifica se a coluna "funcao" está presente nos dados recebidos
-    dados = Usuarios.DeletarUsuarios(codigoUsuario)
-    # Obtém os nomes das colunas
-    column_names = dados.columns
-    # Monta o dicionário com os cabeçalhos das colunas e os valores correspondentes
-    end_data = []
-    for index, row in dados.iterrows():
-        end_dict = {}
-        for column_name in column_names:
-            end_dict[column_name] = row[column_name]
-        end_data.append(end_dict)
-    return jsonify(end_data)
-
-@app.route('/pcp/api/Usuarios/<string:codigo>', methods=['POST'])
-@token_required
-def update_usuario(codigo):
-    # Obtém os dados do corpo da requisição (JSON)
-    data = request.get_json()
-    codigo = str(codigo)
-    # Verifica se a coluna "funcao" está presente nos dados recebidos
-    codigo, nome_ant, senha_ant = Usuarios.ObterUsuariosCodigo(codigo)
-    if codigo == 0:
-        return jsonify({'message': f'Dados do Usuário {codigo} usuario nao existe! ', 'Status': False})
-    else:
-        if 'nome' in data:
-            nome_novo = data['nome']
-        else:
-            nome_novo = nome_ant
-        if 'senha' in data:
-            senha_nova = data['senha']
-        else:
-            senha_nova = senha_ant
-        Usuarios.EditarUsuario(codigo, nome_novo, senha_nova)
-
-        return jsonify({'message': f'Dados do Usuário {codigo} - {nome_novo} atualizado com sucesso'})
 
 @app.route('/pcp/api/Estrutura', methods=['POST'])
 @token_required
