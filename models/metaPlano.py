a70d022f6fc68ce4bf6402f108b0cc2be5bdf4df
luis-fe <luis_engp@hotmail.com>
2023-08-15 14:31:41 -0300

    Commit Plano

--- models/metaPlano.py
+++ models/metaPlano.py
@@ -6,6 +6,8 @@ def Get_Consultar(plano):
     conn = ConexaoPostgreMPL.conexao()
     get = pd.read_sql('select plano, marca, "MetaR$", "Metapç" from pcp."planoMetas" '
                       'where plano = %s ',conn,params=(plano,))
+    get1 = get["Metapç"].sum()
+
 
     get["MetaR$"] = get["MetaR$"].apply(lambda x: "{:,.0f}".format(x))
     get["Metapç"] = get["Metapç"].apply(lambda x: "{:,.0f}".format(x))
@@ -13,7 +15,6 @@ def Get_Consultar(plano):
     get["MetaR$"] = get["MetaR$"].astype(str)
     get["MetaR$"] = 'R$'+get["MetaR$"].str.replace(',', '.')
     get["Metapç"] = get["Metapç"].str.replace(',', '.')
-    get1 = get["Metapç"].sum()
 
     total = pd.DataFrame([{'marca':'TOTAL',"MetaR$" :get1}])
     get = pd.concat([get, total])
