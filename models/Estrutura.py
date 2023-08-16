85db533072b7a6bbf473a2474b3712df4c5f70db
luis-fe <luis_engp@hotmail.com>
2023-08-16 10:25:25 -0300

    Commit Plano

--- models/Estrutura.py
+++ models/Estrutura.py
@@ -127,20 +127,7 @@ def Estrutura(client_ip,plano, pagina=0 ,itensPag=0 , engenharia=SEM_ENGENHARIA,
         return [data]
 
 
-def TemPaginamento(pagina, itensPag, dataframe):
-    if pagina != 0:
-        totalPaginas = dataframe['03- codProduto'].size/itensPag
-        totalPaginas = math.ceil(totalPaginas)
-        totalPaginas = int(totalPaginas)
-        final = pagina * itensPag
-        inicial = (pagina - 1) * itensPag
-        estrutura = dataframe.iloc[inicial:final]
 
-
-        return estrutura, totalPaginas
-    else:
-        estrutura = dataframe
-        return estrutura, False
 
 def TemFiltro(nomedofiltro,dataframe, coluna):
     if nomedofiltro == '0':
