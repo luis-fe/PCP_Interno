3f249605d2bd7c1aad8f105a2696203be309fcf9
luis-fe <luis_engp@hotmail.com>
2023-08-17 09:52:21 -0300

    Commit Plano

---
+++ models/itens.py
@@ -0,0 +1,32 @@
+# Importando os itens para o postgree, acionado via automacao diaria
+import ConexaoCSW
+import pandas as pd
+def ItensCSW():
+    conn = ConexaoCSW.Conexao()
+    itens = pd.read_sql('SELECT i.codigo , i.nome, i2.codCor, i2.codSortimento, i2.codItemPai,'
+                        ' (select t.descricao from tcp.Tamanhos t WHERE t.codEmpresa = 1 and t.sequencia = i2.codSeqTamanho) as tamanho '
+                        ' FROM Cgi.Item i '
+                        ' JOIN Cgi.Item2 i2 on i2.codItem = i.codigo '
+                        " WHERE i.unidadeMedida = 'PC' and i2.Empresa = 1 and i2.codCor > 0",conn)
+    itens['categoria'] = '-'
+    itens['categoria'] = itens.apply(lambda row: Categoria('CAMISA', row['nome'], 'CAMISA', row['categoria']),axis=1)
+    itens['categoria'] = itens.apply(lambda row: Categoria('TSHORT', row['nome'], 'CAMISETA', row['categoria']),axis=1)
+    itens['categoria'] = itens.apply(lambda row: Categoria('POLO', row['nome'], 'POLO', row['categoria']),axis=1)
+    itens['categoria'] = itens.apply(lambda row: Categoria('BABY', row['nome'], 'CAMISETA', row['categoria']),axis=1)
+    itens['categoria'] = itens.apply(lambda row: Categoria('REGATA', row['nome'], 'CAMISETA', row['categoria']),axis=1)
+    itens['categoria'] = itens.apply(lambda row: Categoria('JUST', row['nome'], 'CAMISETA', row['categoria']),axis=1)
+    itens['categoria'] = itens.apply(lambda row: Categoria('BATA', row['nome'], 'CAMISA', row['categoria']),axis=1)
+    itens['categoria'] = itens.apply(lambda row: Categoria('JAQUETA', row['nome'], 'JAQUETA', row['categoria']),axis=1)
+    itens['categoria'] = itens.apply(lambda row: Categoria('SHORT', row['nome'], 'BOARDSHORT', row['categoria']),axis=1)
+    itens['categoria'] = itens.apply(lambda row: Categoria('CARTEIRA', row['nome'], 'CARTEIRA', row['categoria']),axis=1)
+    itens['categoria'] = itens.apply(lambda row: Categoria('MEIA', row['nome'], 'MEIA', row['categoria']),axis=1)
+    itens['categoria'] = itens.apply(lambda row: Categoria('BLAZER', row['nome'], 'JAQUETA', row['categoria']),axis=1)
+
+    return itens
+
+
+def Categoria(contem, valorReferencia, valorNovo, categoria):
+    if contem in valorReferencia:
+        return valorNovo
+    else:
+        return categoria
--- routes/IntegracaoBI_routes.py
+++ routes/IntegracaoBI_routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint,Flask, render_template, jsonify, request
 from functools import wraps
 from flask_cors import CORS
-from models import IntegracaoBI, ResponsabilidadeFase, ObterInfCSW
+from models import IntegracaoBI, ResponsabilidadeFase, ObterInfCSW, itens
 
 integracaoBI = Blueprint('integracaoBI_routes', __name__)
 
@@ -22,6 +22,20 @@ def LoteBI():
 @integracaoBI.route('/pcp/api/FasesBI', methods=['GET'])
 def FasesBI():
     usuarios = ResponsabilidadeFase.ObterFaseResponsais()
+    # Obtém os nomes das colunas
+    column_names = usuarios.columns
+    # Monta o dicionário com os cabeçalhos das colunas e os valores correspondentes
+    OP_data = []
+    for index, row in usuarios.iterrows():
+        op_dict = {}
+        for column_name in column_names:
+            op_dict[column_name] = row[column_name]
+        OP_data.append(op_dict)
+    return jsonify(OP_data)
+
+@integracaoBI.route('/pcp/api/IntensBI', methods=['GET'])
+def IntensBI():
+    usuarios = itens.ItensCSW()
     # Obtém os nomes das colunas
     column_names = usuarios.columns
     # Monta o dicionário com os cabeçalhos das colunas e os valores correspondentes
