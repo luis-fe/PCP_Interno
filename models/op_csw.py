8ce544c8e775c2190bbcdb7be176cd1d384eec3e
luis-fe <luis_engp@hotmail.com>
2023-08-18 12:17:55 -0300

    Commit Plano

---
+++ models/op_csw.py
@@ -0,0 +1,32 @@
+# Importando os itens para o postgree, acionado via automacao diaria
+import ConexaoCSW
+import pandas as pd
+import time
+
+def ItensCSW(i, paginas, anoLote):
+    i = int(i)
+    final = paginas * i
+    conn = ConexaoCSW.Conexao()
+    start_time = time.time()
+
+    itens = pd.read_sql('SELECT top '+str(final)+ ' ot.numeroOP, ot.codProduto , ot.codItem, ot.codSortimento, ot.seqTamanho , ot.qtdePecas1Qualidade, '
+                        ' ot.qtdePecas2Qualidade , ot.qtdePecasImplementadas , op.situacao, op.codFaseAtual, op.codTipoOP, op.dataGeracao, op.codLote t WHERE t.codEmpresa = 1 and t.sequencia = i2.codSeqTamanho) as tamanho '
+                        ' from tco.OrdemProdTamanhos ot '
+                        ' join tco.OrdemProd op on op.codEmpresa = ot.codEmpresa and ot.numeroOP = op.numeroOP  '
+                        " WHERE ot.codEmpresa = 1 and op.codLote  like '"+anoLote+"'",conn)
+    end_time = time.time()
+    execution_time = end_time - start_time
+    execution_time = round(execution_time, 2)
+    execution_time = str(execution_time)
+    ConexaoCSW.ControleRequisicao('Consultar OPs Csw', execution_time, f'powerbi numero de intens {final}')
+
+
+
+
+
+    inicial = (paginas - 1) * i
+    itens = itens.iloc[inicial:final]
+
+    return itens
+
+
--- routes/IntegracaoBI_routes.py
+++ routes/IntegracaoBI_routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint,Flask, render_template, jsonify, request
 from functools import wraps
 from flask_cors import CORS
-from models import IntegracaoBI, ResponsabilidadeFase, ObterInfCSW, itens
+from models import IntegracaoBI, ResponsabilidadeFase, ObterInfCSW, itens, op_csw
 
 integracaoBI = Blueprint('integracaoBI_routes', __name__)
 
@@ -41,6 +41,25 @@ def IntensBI():
     data = request.args.get('dataInicial', '2015-01-01')
 
     usuarios = itens.ItensCSW(topItem, int(paginas),orderby,data)
+
+    # Obtém os nomes das colunas
+    column_names = usuarios.columns
+    # Monta o dicionário com os cabeçalhos das colunas e os valores correspondentes
+    OP_data = []
+    for index, row in usuarios.iterrows():
+        op_dict = {}
+        for column_name in column_names:
+            op_dict[column_name] = row[column_name]
+        OP_data.append(op_dict)
+    return jsonify(OP_data)
+
+@integracaoBI.route('/pcp/api/OPSBI', methods=['GET'])
+def OPSBI():
+    topItem = request.args.get('itens')
+    paginas = request.args.get('paginas',1)
+    data = request.args.get('anoLote', '23')
+
+    usuarios = op_csw.ItensCSW(topItem, int(paginas),data)
 
     # Obtém os nomes das colunas
     column_names = usuarios.columns
