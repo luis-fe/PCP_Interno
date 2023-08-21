f1916eb516c903655d40d5b2249db7d832ffe8c9
luis-fe <luis_engp@hotmail.com>
2023-08-21 14:40:31 -0300

    Commit Plano

--- routes/responsabilidadeFase_routes.py
+++ routes/responsabilidadeFase_routes.py
@@ -18,7 +18,7 @@ def token_required(f):
 @token_required
 def get_ResponsabilidadeFase_routes():
     novo_usuario = request.get_json()
-    nomeFase = novo_usuario.get('nomeFase', '0')
+    nomeFase = novo_usuario.get('nomeFase', 0)
     responsavel = novo_usuario.get('responsavel', '0')
     codFase = novo_usuario.get('codFase', '0')
 
