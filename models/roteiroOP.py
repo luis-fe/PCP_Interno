13e9454129a31edcb3181fcf22bfe2fb9a2f3855
luis-fe <luis_engp@hotmail.com>
2023-09-18 13:49:19 -0300

    Commit Plano

--- models/roteiroOP.py
+++ models/roteiroOP.py
@@ -9,10 +9,15 @@ def Roteiro(like, empresa):
     query = pd.read_sql('select top 200 op.codLote, op.numeroOP, op.situacao, op.codFaseAtual, op.codSeqRoteiroAtual '
                         ' FROM tco.OrdemProd op '
                         ' WHERE op.codEmpresa = '+empresa+" and op.situacao > 0 and op.codLote like "+ like, conn )
+
+
+    query2 = pd.read_sql('SELECT top 1000 r.codLote , r.numeroOP , r.codSeqRoteiro , r.codFase  from tco.RoteiroOP r '
+                         ' WHERE op.codEmpresa = '+empresa+" and op.situacao > 0 and op.codLote like "+ like, conn )
+
     conn.close()
     query.fillna('-', inplace=True)
-    query["situacaoMovOP"] = query.apply(lambda row: 'em processo' if row['codFaseAtual'] != '-' else '-', axis=1)
 
+    query = pd.merge(query, query2, on='numeroOP')
 
     return query
 
