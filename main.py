2b95013715b54b5b4e73795627325edcded64b9a
luis-fe <luisengp@gmail.com>
2023-09-26 16:21:14 -0300

    Commit Plano

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from models import ABC_PLANO, CalendarioProducao, AutomacaoSugestaoPedidos, Obte
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
-port = int(os.environ.get('PORT', 8001))
+port = int(os.environ.get('PORT', 8000))
 
 app.register_blueprint(routes_blueprint)
 #Aqui registo todas as rotas , url's DO PROJETO, para acessar bastar ir na pasta "routes",
