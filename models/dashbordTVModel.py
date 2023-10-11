c464c84180539e34fee9b43952b21c13bcab909d
luis-fe <luisengp@gmail.com>
2023-10-11 09:59:31 -0300

    Commit Plano

--- models/dashbordTVModel.py
+++ models/dashbordTVModel.py
@@ -13,11 +13,13 @@ def Faturamento_ano(ano, empresa):
                             'and n.dataEmissao <= ' + "'" + dataFim + "'" + 'and situacao = 2 ',conn)
     # Filtrando os dados de janeiro
     Janeiro = dataframe[dataframe['dataEmissao'].str.contains('-01-')]
+    Fevereiro = dataframe[dataframe['dataEmissao'].str.contains('-02-')]
 
     # Calculando a soma dos valores faturados em janeiro
     Janeiro_faturado = Janeiro['faturado'].sum()
+    Fevereiro_faturado = Fevereiro['faturado'].sum()
 
     # Criando um DataFrame com o resultado
-    df_janeiro = pd.DataFrame({'Mês': ['Janeiro'], 'Faturado': [Janeiro_faturado]})
+    df_janeiro = pd.DataFrame({'Mês': ['01 - Janeiro', '02 - Fevereiro'], 'Faturado': [Janeiro_faturado, Fevereiro_faturado]})
 
     return df_janeiro
