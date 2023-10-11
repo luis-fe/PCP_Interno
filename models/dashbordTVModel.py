4699f4e5bdb0c92530918b62deeeb6927413d3bc
luis-fe <luisengp@gmail.com>
2023-10-11 10:08:34 -0300

    Commit Plano

--- models/dashbordTVModel.py
+++ models/dashbordTVModel.py
@@ -2,24 +2,35 @@ import pandas as pd
 import ConexaoCSW
 import ConexaoPostgreMPL
 
-
 def Faturamento_ano(ano, empresa):
+    conn = ConexaoCSW.Conexao()
     dataInicio = ano + '-01-01'
     dataFim = ano + '-12-31'
-    conn = ConexaoCSW.Conexao()
-    dataframe = pd.read_sql('select n.codTipoDeNota as tiponota, n.dataEmissao, n.vlrTotal as faturado'
-                            '  FROM Fat.NotaFiscal n '
-                            'where n.codEmpresa = ' + empresa + ' and n.codPedido >= 0 and n.dataEmissao >= ' + "'" + dataInicio + "'" + ' '
-                            'and n.dataEmissao <= ' + "'" + dataFim + "'" + 'and situacao = 2 ',conn)
-    # Filtrando os dados de janeiro
-    Janeiro = dataframe[dataframe['dataEmissao'].str.contains('-01-')]
-    Fevereiro = dataframe[dataframe['dataEmissao'].str.contains('-02-')]
 
-    # Calculando a soma dos valores faturados em janeiro
-    Janeiro_faturado = Janeiro['faturado'].sum()
-    Fevereiro_faturado = Fevereiro['faturado'].sum()
+    query = 'select n.codTipoDeNota as tiponota, n.dataEmissao, n.vlrTotal as faturado ' \
+            'FROM Fat.NotaFiscal n ' \
+            'where n.codEmpresa = ' + empresa + ' and n.codPedido >= 0 ' \
+            'and n.dataEmissao >= ' + "'" + dataInicio + "'" + ' ' \
+            'and n.dataEmissao <= ' + "'" + dataFim + "'" + ' and situacao = 2 '
 
-    # Criando um DataFrame com o resultado
-    df_janeiro = pd.DataFrame({'Mês': ['01 - Janeiro', '02 - Fevereiro'], 'Faturado': [Janeiro_faturado, Fevereiro_faturado]})
+    dataframe = pd.read_sql(query, conn)
 
-    return df_janeiro
+    meses = ['01 - Janeiro', '02 - Fevereiro', '03 - Março', '04 - Abril', '05 - Maio', '06 - Junho',
+             '07 - Julho', '08 - Agosto', '09 - Setembro', '10 - Outubro', '11 - Novembro', '12 - Dezembro']
+
+    faturamento_por_mes = []
+
+    for mes in meses:
+        # Filtrar os dados do mês atual
+        data_atual = f'{ano}-{mes.split(" - ")[1]}-'
+        filtro = (dataInicio <= data_atual) & (data_atual <= dataFim)
+        df_mes = dataframe[filtro]
+
+        # Calcular o faturamento do mês
+        faturamento_mes = df_mes['faturado'].sum()
+        faturamento_por_mes.append(faturamento_mes)
+
+    # Criar um DataFrame com os resultados
+    df_faturamento = pd.DataFrame({'Mês': meses, 'Faturado': faturamento_por_mes})
+
+    return df_faturamento
