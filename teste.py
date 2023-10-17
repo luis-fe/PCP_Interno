1e261841236500fb6024cc20cabc2041bb742f9a
luis-fe <luisengp@gmail.com>
2023-10-17 17:23:07 -0300

    Commit Plano

--- teste.py
+++ teste.py
@@ -1,26 +1,10 @@
 import pandas as pd
 
-# Seu DataFrame original
-data = {'produto': ['a', 'b', 'b', 'c', 'c','c', 'e'],
-        'necessidade': [4, 1, 2, 3, 1, 2, 1],
-        'estoque': [10, 2, 2, 4, 4, 4, 1]}
+# Exemplo de DataFrame com a coluna "coluna1"
+data = {'coluna1': ['aa', 'bb', 'cc', 'dd']}
 df = pd.DataFrame(data)
 
-# Ordene o DataFrame pelo produto para garantir que o cálculo seja feito corretamente
-df = df.sort_values(by=['produto'])
+# Transformar os valores da coluna em uma única string
+resultado = '({})'.format(', '.join(["'{}'".format(valor) for valor in df['coluna1']]))
 
-
-
-# Calcule o estoque novo com base na necessidade e no estoque atual
-for idx, row in df.iterrows():
-    produto = row['produto']
-    necessidade = row['necessidade']
-    estoque_atual = row['estoque']
-
-
-    # Atualize o estoque da próxima linha com o valor calculado
-    if idx < len(df) - 1 and df.at[idx + 1, 'produto'] == produto:
-        df.at[idx + 1, 'estoque'] = max(0, estoque_atual - necessidade)
-
-# Exiba o DataFrame resultante
-print(df)
+print(type(resultado))
