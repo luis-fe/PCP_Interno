01385477f39cc85a4e5aa02975d17a94473cb15e
luis-fe <luisengp@gmail.com>
2023-10-18 14:12:40 -0300

    Commit Plano

--- teste.py
+++ teste.py
@@ -1,7 +1,4 @@
 from flask import Flask, render_template
-import dash_html_components as html
-import dash_core_components as dcc
-import plotly.express as px
 import pandas as pd
 
 app = Flask(__name__)
