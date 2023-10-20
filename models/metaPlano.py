96f91eba5012d1efb5a80ccbe4d7f2b5714f2546
luis-fe <luisengp@gmail.com>
2023-10-20 14:50:40 -0300

    Commit Plano

--- models/metaPlano.py
+++ models/metaPlano.py
@@ -121,7 +121,7 @@ def metasSemanais(plano):
 
         data['1.1- PACO pçs'] = (data['1- PACO %dist.']/100)*totalpçs
         data["1.1- PACO pçs"] = data["1.1- PACO pçs"].apply(lambda x: "{:,.0f}".format(x))
-
+        data['1.1- PACO pçs'] = data['1.1- PACO pçs'].str.replace(',', '.')
 
         data['1.2- PACO R$'] = (data['1- PACO %dist.']/100)*totalreais
         data["1.2- PACO R$"] = data['1.2- PACO R$'].apply(lambda x: "{:,.2f}".format(x))
