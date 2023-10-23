aaffec7c335ba4250541b03ade265a8b16ebe2c4
luis-fe <luisengp@gmail.com>
2023-10-23 10:45:24 -0300

    Commit Plano

--- models/Vendas.py
+++ models/Vendas.py
@@ -494,17 +494,18 @@ def VendasPlano(plano, empresa, somenteAprovados, Marca):
 
 
 
-    def format_with_separator(value):
-        return locale.format('%0.2f', value, grouping=True)
+    def format_with_separator(value, numeroCasas):
+        numeroCasas = '%0.'+str(numeroCasas)+'f'
+        return locale.format(numeroCasas, value, grouping=True)
 
 
 
-    Pedido['vlrPedido'] = Pedido['vlrPedido'].apply(format_with_separator)
+    Pedido['vlrPedido'] = Pedido['vlrPedido'].apply(format_with_separator,2)
     Pedido['vlrPedido'] = Pedido['vlrPedido'].str.replace('.', ';')
     Pedido['vlrPedido'] = Pedido['vlrPedido'].str.replace(',', '.')
     Pedido['vlrPedido'] = 'R$'+Pedido['vlrPedido'].str.replace(';', ',')
 
-    Pedido['qtdPecasPedido'] = Pedido['qtdPecasPedido'].apply(format_with_separator)
+    Pedido['qtdPecasPedido'] = Pedido['qtdPecasPedido'].apply(format_with_separator,0)
     Pedido['qtdPecasPedido'] = Pedido['qtdPecasPedido'].str.replace('.', ';')
     Pedido['qtdPecasPedido'] = Pedido['qtdPecasPedido'].str.replace(',', '.')
     Pedido['qtdPecasPedido'] = Pedido['qtdPecasPedido'].str.replace(';', ',')
