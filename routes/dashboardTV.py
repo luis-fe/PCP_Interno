7c3f271b0f4cbbbf8d9877735d370c876f9dd88d
luis-fe <luisengp@gmail.com>
2023-11-28 09:14:38 -0300

    Commit Plano.

--- routes/dashboardTV.py
+++ routes/dashboardTV.py
@@ -65,6 +65,7 @@ def AcompVendas():
     Marca = request.args.get('Marca', 'Geral')
     congelado = request.args.get('congelado', False)
     print(congelado)
+    print(congelado.astype)
     plano = Vendas.VendasPlano(plano,empresa,somenteAprovados,Marca,bool(congelado))
     plano = pd.DataFrame(plano)
     column_names = plano.columns
