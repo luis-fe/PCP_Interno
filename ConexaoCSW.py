c21181523bd25991e08d2310d9e433d138f43d77
luis-fe <luisengp@gmail.com>
2023-12-05 17:32:39 -0300

    corrigindo a fonte

--- ConexaoCSW.py
+++ ConexaoCSW.py
@@ -11,7 +11,7 @@ def obterHoraAtual():
 def Conexao():
     conn = jaydebeapi.connect(
     'com.intersys.jdbc.CacheDriver',
-    'jdbc:Cache://192.168.0.25:1972/CONSISTEMs',
+    'jdbc:Cache://192.168.0.25:1972/CONSISTEM',
     {'user': 'root', 'password': 'ccscache'},
     'CacheDB.jar'
 )
