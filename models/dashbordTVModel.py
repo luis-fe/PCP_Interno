caa0416ee38dad2f2f9baa760d4b98ad10203a58
luis-fe <luisengp@gmail.com>
2023-12-06 07:57:48 -0300

    corrigindo a fonte

--- models/dashbordTVModel.py
+++ models/dashbordTVModel.py
@@ -366,7 +366,7 @@ def Backup(ano, empresa):
     elif empresa == 'Outras':
         query = 'select n.codTipoDeNota as tiponota, n.dataEmissao, n.vlrTotal as faturado, codPedido ' \
                 'FROM Fat.NotaFiscal n ' \
-                'where n.codTipoDeNota in (48, 167, 30, 118, 102) ' \
+                'where n.codTipoDeNota in (48, 167, 30, 118, 102, 149, 168, 170) ' \
                 'and n.dataEmissao >= ' + "'" + dataInicio + "'" + ' ' \
                                                                    'and n.dataEmissao <= ' + "'" + dataFim + "'" + ' and situacao = 2 and codempresa in(1, 4) '
         dataframe = pd.read_sql(query, conn)
