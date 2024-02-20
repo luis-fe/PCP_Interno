2442857ac630226d0c0014d96d1ce06e1d46665e
luis-fe <luisengp@gmail.com>
2024-02-20 12:45:10 -0300

    get fases por categoria

--- models/CargaOPs.py
+++ models/CargaOPs.py
@@ -130,10 +130,11 @@ def OPemProcesso(empresa, AREA, filtro = '-'):
 
 
         consulta = pd.merge(consulta,leadTime2,on=['codFase','categoria'], how='left')
-        consulta.fillna('-', inplace=True)
+        consulta['meta2'].fillna(0, inplace=True)
         consulta['meta2'] = consulta['meta2'].astype(int)
 
-        consulta['meta'] = consulta.apply(lambda row : row['meta'] if row['meta2'] == '-' else row['meta2'], axis=1)
+
+        consulta['meta'] = consulta.apply(lambda row : row['meta'] if row['meta2'] == 0 else row['meta2'], axis=1)
         consulta.drop('meta2', axis=1, inplace=True)
 
 
