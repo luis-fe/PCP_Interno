406feb82b35ff4c4c5c98cffba36e6970ff422cb
luis-fe <luisengp@gmail.com>
2024-02-21 09:56:40 -0300

    get fases por categoria

--- models/CargaOPs.py
+++ models/CargaOPs.py
@@ -175,7 +175,7 @@ def OPemProcesso(empresa, AREA, filtro = '-'):
         consulta['filtro'] = consulta['codProduto']+consulta['data_entrada']+consulta['categoria']+consulta['codFase']+'-'+consulta['nomeFase']+consulta['numeroOP']+consulta['responsavel']+consulta['status']
         consulta['filtro'] = consulta['filtro'].str.replace(' ', '')
 
-
+        consulta.drop(['justificativa2','justificativa1','seqRoteiro'], axis=1, inplace=True)
         consulta.to_csv('cargaOP.csv',index=True)
 
         consulta = consulta[consulta['Area']== AREA]
