2db047f6e493cc4707b5c40654c08fca9ff7e72b
luis-fe <luisengp@gmail.com>
2024-02-21 10:23:09 -0300

    get fases por categoria

--- models/CargaOPs.py
+++ models/CargaOPs.py
@@ -167,7 +167,7 @@ def OPemProcesso(empresa, AREA, filtro = '-'):
         consulta.drop('meta2', axis=1, inplace=True)
 
         consulta['status'] = consulta.apply(lambda row: '2-Atrasado' if row['dias na Fase'] > row['meta'] else '0-Normal',axis=1 )
-        consulta['status'] = consulta.apply(lambda row: '1-Atencao' if row['status'] == '0-Atrasado' and row['dias na Fase'] < row['limite_atencao']  else row['status'],axis=1 )
+        consulta['status'] = consulta.apply(lambda row: '1-Atencao' if row['status'] == '2-Atrasado' and row['dias na Fase'] < row['limite_atencao']  else row['status'],axis=1 )
 
         consulta = consulta.sort_values(by=['status','dias na Fase'], ascending=False)  # escolher como deseja classificar
 
