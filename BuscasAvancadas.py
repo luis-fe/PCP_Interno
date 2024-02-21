ba6c980cafb4719c48256fde1178a005fec90e72
luis-fe <luisengp@gmail.com>
2024-02-21 09:33:38 -0300

    get fases por categoria

--- BuscasAvancadas.py
+++ BuscasAvancadas.py
@@ -1,10 +1,12 @@
+
 # Velocidade de pesquisa
+
 
 ## SQL BUSCANDO AS ORDEM DE PRODUCAO EM ABERTO - velocidade media: 0,850 s (otima)
 
 def OP_Aberto():
-    OP_emAberto = 'SELECT dataInicio as startOP, codProduto  , numeroOP , codTipoOP , codFaseAtual as codFase , codSeqRoteiroAtual as seqAtual, codPrioridadeOP, codLote , codEmpresa, '\ 
-                   ' (SELECT f.nome from tcp.FasesProducao f WHERE f.codempresa = 1 and f.codfase = o.codFaseAtual) as nomeFase, ' \
+    OP_emAberto = 'SELECT dataInicio as startOP, codProduto  , numeroOP , codTipoOP , codFaseAtual as codFase , codSeqRoteiroAtual as seqAtual, ' \
+                  'codPrioridadeOP, codLote , codEmpresa, (SELECT f.nome from tcp.FasesProducao f WHERE f.codempresa = 1 and f.codfase = o.codFaseAtual) as nomeFase, ' \
                   '(select e.descricao from tcp.Engenharia e WHERE e.codempresa = o.codEmpresa and e.codengenharia = o.codProduto) as descricao' \
                   ' FROM tco.OrdemProd o '\
                    ' WHERE o.codEmpresa = 1 and o.situacao = 3'
