e52f80c6d6c0499996c82e3161fd5d9f5b8aaef7
luis-fe <luisengp@gmail.com>
2024-02-21 09:29:22 -0300

    get fases por categoria

--- BuscasAvancadas.py
+++ BuscasAvancadas.py
@@ -4,11 +4,12 @@
 
 def OP_Aberto():
     OP_emAberto = 'SELECT dataInicio as startOP, codProduto  , ' \
-                  'numeroOP , codTipoOP , codFaseAtual as codFase , codSeqRoteiroAtual as seqAtual, codPrioridadeOP, codLote , codEmpresa, ' \ 
+                  'numeroOP , codTipoOP , codFaseAtual as codFase , codSeqRoteiroAtual as seqAtual, codPrioridadeOP, codLote , codEmpresa, '\ 
                    ' (SELECT f.nome from tcp.FasesProducao f WHERE f.codempresa = 1 and f.codfase = o.codFaseAtual) as nomeFase, ' \
                   '(select e.descricao from tcp.Engenharia e WHERE e.codempresa = o.codEmpresa and e.codengenharia = o.codProduto) as descricao' \
                   ' FROM tco.OrdemProd o '\
                    ' WHERE o.codEmpresa = 1 and o.situacao = 3'
+
     return OP_emAberto
 
 
