3e83364c84e2b47739089a20e563113ed8848540
luis-fe <luisengp@gmail.com>
2024-02-27 16:34:39 -0300

    prioridade Urgente!

--- models/CargaOPs.py
+++ models/CargaOPs.py
@@ -200,7 +200,7 @@ def OPemProcesso(empresa, AREA, filtro = '-'):
         consulta['status'] = consulta.apply(lambda row: '2-Atrasado' if row['dias na Fase'] > row['meta'] else '0-Normal',axis=1 )
         consulta['status'] = consulta.apply(lambda row: '1-Atencao' if row['status'] == '2-Atrasado' and row['dias na Fase'] < row['limite_atencao']  else row['status'],axis=1 )
 
-        consulta = consulta.sort_values(by=['status','dias na Fase'], ascending=False)  # escolher como deseja classificar
+        consulta = consulta.sort_values(by=['prioridade','status','dias na Fase'], ascending=False)  # escolher como deseja classificar
 
         consulta['filtro'] = consulta['codProduto']+consulta['data_entrada']+consulta['categoria']+consulta['codFase']+'-'+consulta['nomeFase']+consulta['numeroOP']+consulta['responsavel']+consulta['status']
         consulta['filtro'] = consulta['filtro'].str.replace(' ', '')
