94daed349b35a845d32bc2199726a33f6dcbdbb0
luis-fe <luisengp@gmail.com>
2024-02-29 15:38:55 -0300

    incrementando o filtro diferente

--- models/CargaOPs.py
+++ models/CargaOPs.py
@@ -5,6 +5,8 @@ import datetime
 import pytz
 import ConexaoPostgreMPL
 import BuscasAvancadas
+import locale
+
 def ResponsabilidadeFases():
     conn = ConexaoPostgreMPL.conexao()
 
@@ -67,6 +69,11 @@ def OPemProcesso(empresa, AREA, filtro = '-', filtroDiferente = '', tempo = 9999
         pcs = pd.read_sql(
             'select numeroop as "numeroOP", total_pcs as "Qtd Pcs" from "Reposicao".off.ordemprod ',
             conn3)
+        def format_with_separator(value):
+
+                return locale.format('%0.0f', value, grouping=True)
+
+        pcs['Qtd Pcs'] = pcs['Qtd Pcs'].apply(format_with_separator)
 
         pcs['Qtd Pcs'].fillna(0, inplace=True)
         pcs['Qtd Pcs'] =pcs['Qtd Pcs'] .astype(int)
