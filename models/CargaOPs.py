5005ad3aa31e539a60cd6d9dd799ed5cbb573888
luis-fe <luisengp@gmail.com>
2024-03-05 11:54:43 -0300

    incrementando o filtro diferente

--- models/CargaOPs.py
+++ models/CargaOPs.py
@@ -58,10 +58,12 @@ def OPemProcesso(empresa, AREA, filtro = '-', filtroDiferente = '', tempo = 9999
 
         partes['nomeParte']= partes.apply(
             lambda row: NomePartes(row['nomeParte'],'BORDADO','bor'), axis=1)
-        partes['nomeParte']= partes.apply(
-            lambda row: NomePartes(row['nomeParte'],'SILK','Silk'), axis=1)
+
         partes['nomeParte']= partes.apply(
             lambda row: NomePartes(row['nomeParte'],'COSTAS','SCostas'), axis=1)
+
+        partes['nomeParte']= partes.apply(
+            lambda row: NomePartes(row['nomeParte'],'SILK','Silk'), axis=1)
 
         partes['codNatEstoque'] = partes['nomeParte']
         partes.drop('nomeParte', axis=1, inplace=True)
