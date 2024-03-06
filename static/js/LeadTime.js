7fb1c3bb659a2c3b54c5951d9647bfe815d9c196
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2024-03-06 16:23:22 -0300

    Update LeadTime.js

--- static/js/LeadTime.js
+++ static/js/LeadTime.js
@@ -11,6 +11,7 @@ const InputNaoContem = document.getElementById('InputNaoContem');
 const ConfirmarFiltro = document.getElementById('ConfirmarFiltro');
 const BotaoExcel = document.getElementById('BotaoExcel');
 const LabelOpsNormais = document.getElementById('LabelOpsNormais');
+const LabelQtd = document.getElementById('LabelQtd');
 const LabelOpsAtencao = document.getElementById('LabelOpsAtencao');
 const LabelOpsAtrasadas = document.getElementById('LabelOpsAtrasadas');
 const LabelOps = document.getElementById('LabelOps');
@@ -21,6 +22,8 @@ const InputObs = document.getElementById('InputJustificativa');
 const FecharJustificativa = document.getElementById('FecharJustificativa');
 const SalvarObs = document.getElementById('SalvarObs');
 const modalLoading = document.getElementById("ModalLoading");
+const ModalPendencia = document.getElementById("ModalPendencia");
+const DivPendencias = document.getElementById("DivPendencias");
 let VarOp = '';
 let VarFase = '';
 let VarObs = '';
@@ -52,7 +55,7 @@ function atualizarJustificativaBotao(op, fase, justificativa) {
 }
 
 async function OpGetObs(Op, Fase) {
-    
+
     try {
         const response = await fetch(`http://192.168.0.183:8000/pcp/api/ConsultarJustificativa?ordemProd=${Op}&fase=${Fase}`, {
             method: 'GET',
@@ -68,11 +71,11 @@ async function OpGetObs(Op, Fase) {
 
         } else {
             throw new Error('Erro No Retorno');
-            
+
         }
     } catch (error) {
         console.error(error);
-        
+
     }
 }
 
@@ -97,21 +100,22 @@ async function ConsultaOp(Api, Empresa, Filtro, Classificacao) {
         if (response.ok) {
             modalLoading.style.display = 'none'
             const data = await response.json();
-            console.log(data);
             const DetalhamentoApi = data[0]['3 -Detalhamento'];
             container.innerHTML = '';
             LabelOps.innerHTML = `Qtd. Op's Abertas: <br> ${parseFloat(data[0]['1-Total OP']).toLocaleString('pt-BR')}`;
-            LabelOpsNormais.innerHTML = `Qtd. Op's Normais: <br> ${parseInt(data[0]['1-Total OP']) - parseInt(data[0]['2- OPs Atrasadas']) - parseInt(data[0]['2.1- OPs Atencao'])} Ops`;
+            LabelOpsNormais.innerHTML = `Qtd. Op's no Prazo: <br> ${parseInt(data[0]['1-Total OP']) - parseInt(data[0]['2- OPs Atrasadas']) - parseInt(data[0]['2.1- OPs Atencao'])} Ops`;
             LabelOpsAtencao.innerHTML = `Qtd. Op's em Atenção: <br> ${data[0]['2.1- OPs Atencao']}`;
             LabelOpsAtrasadas.innerHTML = `Qtd. Op's Atrasadas: <br> ${data[0]['2- OPs Atrasadas']}`;
+            LabelQtd.innerHTML = `Qtd. Peças: <br> ${parseFloat(data[0]['0-Total DE pçs']).toLocaleString('pt-BR')}`;
+
 
             await DetalhamentoApi.forEach(dado => {
                 const botao = document.createElement('button');
-            
+
                 botao.classList.add('botao');
                 botao.setAttribute('data-op', dado.numeroOP);
                 botao.setAttribute('data-fase', dado.codFase);
-            
+
                 if (dado.prioridade === '1-URGENTE') {
                     const urgenteSymbol = document.createElement('span');
                     urgenteSymbol.innerHTML = '🚨'; // Símbolo de ponto de exclamação
@@ -119,21 +123,36 @@ async function ConsultaOp(Api, Empresa, Filtro, Classificacao) {
                     urgenteSymbol.style.fontSize = '30px';
                     botao.appendChild(urgenteSymbol);
                 }
-            
+
                 const conteudoBotao = document.createElement('strong');
+
                 conteudoBotao.innerHTML = `
-                    <span class="CodFase">Fase: ${dado.codFase} - ${dado.nomeFase}</span><br>
-                    <span class="NumeroOp">OP:${dado.numeroOP} / Qtd: ${dado['Qtd Pcs']} Pçs</span><br>
-                    <span class="Engenharias">Tipo Op: ${dado.codTipoOP}</span><br>
-                    <span class="Engenharias">${dado.descricao}</span><br>
-                    Responsável: ${dado.responsavel}<br>
-                    Meta: ${dado.meta} dias<br>
-                    Dias na Fase: ${dado['dias na Fase']}<br>
-                    <span class="Justificativa">Justificativa: ${dado.justificativa}</span>
-                `;
-            
+                <span class="CodFase">Fase: ${dado.codFase} - ${dado.nomeFase}</span><br>
+                <span class="NumeroOp">OP:${dado.numeroOP} / Qtd: ${dado['Qtd Pcs']} Pçs</span><br>
+                <span class="NumeroOp">Engenharia: ${dado.codProduto}</span><br>
+                <span class="Engenharias">${dado.descricao}</span><br>
+                <span class="Engenharias">Tipo Op: ${dado.codTipoOP}</span><br>
+                <span class="Engenharias">Responsável: ${dado.responsavel}<br>
+                <span class="Engenharias">Meta: ${dado.meta} dias<br>
+                <span class="Engenharias">Dias na Fase: ${dado['dias na Fase']}<br>
+                <span class="Justificativa">Justificativa: ${dado.justificativa}</span>
+                <span style="${dado['Status Aguardando Partes'] === 'PENDENTE' || dado['Status Aguardando Partes'] === 'OK' ? ' font-size: 50px; display: flex; flex-direction: column; justify-content: center; align-items: center; text-align: center;' : ''}">
+                    ${dado['Status Aguardando Partes'] === 'PENDENTE' ? '⚠️' : ''}
+                    ${dado['Status Aguardando Partes'] === 'OK' ? '✅' : ''}
+                </span>
+            `;
+            botao.onmouseover = () => {
+                if (dado['Status Aguardando Partes'] === 'PENDENTE') {
+                    mostrarModalPendente(botao, dado);
+                }
+            };
+
+            botao.onmouseout = () => {
+                fecharModalPendente();
+            };
+
                 botao.appendChild(conteudoBotao);
-                
+
                 switch (dado.status) {
                     case '2-Atrasado':
                         botao.style.backgroundColor = 'red';
@@ -148,28 +167,51 @@ async function ConsultaOp(Api, Empresa, Filtro, Classificacao) {
                     // Adicione outros casos conforme necessário
                 }
 
-                
+
                 botao.addEventListener('click', async () => {
                     await OpGetObs(dado.numeroOP, dado.codFase);
-                    console.log(dado);
                     LabelOp.innerHTML = `OP: ${dado.numeroOP}`;
                     InputObs.value = `${VarObs}`;
                     InputObs.focus();
-                    
+
                     // Calcular a posição considerando a barra de rolagem
                     var rect = botao.getBoundingClientRect();
                     var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-                    
+
                     ModalObs.style.top = rect.bottom + scrollTop + 'px';
                     ModalObs.style.left = rect.left + 'px';
                     ModalObs.style.display = 'block';
-                
+
                     VarOp = dado.numeroOP;
                     VarFase = dado.codFase;
                 });
                 container.appendChild(botao);
+
+                function mostrarModalPendente(botao, dado) {
+                    var rect = botao.getBoundingClientRect();
+                    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+                    const Pendencia = dado.estaPendente
+                    ModalPendencia.style.top = rect.bottom + scrollTop + 'px';
+                    ModalPendencia.style.left = rect.left + 'px';
+                    ModalPendencia.style.display = 'block';
+
+                    Pendencia.forEach(Pendencia => {
+                        const label = document.createElement('label');
+                        label.textContent = Pendencia;
+                        label.style.fontSize = '22px'
+                        DivPendencias.appendChild(label);
+                    })
+                }
+                
+                function fecharModalPendente() {
+                    // Oculta o modal
+                    ModalPendencia.style.display = 'none';
+                
+                    // Limpa o conteúdo dentro de DivPendencias
+                    DivPendencias.innerHTML = '';
+                }
             });
-            
+
         } else {
             modalLoading.style.display = 'none'
             throw new Error('Erro No Retorno');
@@ -180,13 +222,15 @@ async function ConsultaOp(Api, Empresa, Filtro, Classificacao) {
     }
 }
 
+
+
 SalvarObs.addEventListener('click', async () => {
     dadoApi = {
         "ordemProd": VarOp,
         "fase": VarFase,
         "justificativa": InputObs.value
     };
-    
+
     await fetch(ApiJustificativa, {
         method: 'PUT',
         headers: {
@@ -195,23 +239,23 @@ SalvarObs.addEventListener('click', async () => {
         },
         body: JSON.stringify(dadoApi),
     })
-    .then(response => {
-        if (response.ok) {
-            alert('Justificativa salva com Sucesso');
-            InputObs.value = ''; // Limpar o conteúdo do InputObs
-            ModalObs.style.display = 'none'; // Certifique-se de que ModalObs está acessível
-            atualizarJustificativaBotao(VarOp, VarFase, dadoApi.justificativa);
-            return response.json();
-        } else {
-            throw new Error('Não foi possível Salvar a Justificativa');
-        }
-    })
-    .then(data => {
-        console.log(data);
-    })
-    .catch(error => {
-        console.error(error);
-    });
+        .then(response => {
+            if (response.ok) {
+                alert('Justificativa salva com Sucesso');
+                InputObs.value = ''; // Limpar o conteúdo do InputObs
+                ModalObs.style.display = 'none'; // Certifique-se de que ModalObs está acessível
+                atualizarJustificativaBotao(VarOp, VarFase, dadoApi.justificativa);
+                return response.json();
+            } else {
+                throw new Error('Não foi possível Salvar a Justificativa');
+            }
+        })
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.error(error);
+        });
 });
 
 
@@ -247,7 +291,7 @@ async function ExportarExcel(Api, Empresa, Filtro) {
         } else {
             throw new Error('Erro No Retorno');
         }
-     } catch (error) {
+    } catch (error) {
         console.error(error);
     }
 }
@@ -264,7 +308,7 @@ ConfirmarFiltro.addEventListener('click', () => {
 });
 
 window.addEventListener('load', () => {
-    ConsultaOp(ApiConsulta, '1', '', 'prioridade');
+    ConsultaOp(ApiConsulta, '1', '', '');
 });
 
 var valorSelecionado = document.getElementById("btnLeadTime").getAttribute("data-valor");
@@ -272,17 +316,34 @@ var valorSelecionado = document.getElementById("btnLeadTime").getAttribute("data
 
 document.getElementById("btnLeadTime").classList.add("botao-selecionado");
 
-    // Função para selecionar o botão e deselecionar o outro
-     function selecionarBotao(botaoClicado) {
-        var btnUrgente = document.getElementById("btnUrgente");
-        var btnLeadTime = document.getElementById("btnLeadTime");
+// Função para selecionar o botão e deselecionar o outro
+function selecionarBotao(botaoClicado) {
+    var btnUrgente = document.getElementById("btnUrgente");
+    var btnLeadTime = document.getElementById("btnLeadTime");
 
-        if (botaoClicado === btnUrgente) {
-            btnUrgente.classList.add("botao-selecionado");
-            btnLeadTime.classList.remove("botao-selecionado");
-            valorSelecionado = botaoClicado.getAttribute("data-valor");
-        } else {
-            btnUrgente.classList.remove("botao-selecionado");
-            btnLeadTime.classList.add("botao-selecionado");
-            valorSelecionado = botaoClicado.getAttribute("data-valor");
-        }}
+    if (botaoClicado === btnUrgente) {
+        btnUrgente.classList.add("botao-selecionado");
+        btnLeadTime.classList.remove("botao-selecionado");
+        valorSelecionado = botaoClicado.getAttribute("data-valor");
+    } else {
+        btnUrgente.classList.remove("botao-selecionado");
+        btnLeadTime.classList.add("botao-selecionado");
+        valorSelecionado = botaoClicado.getAttribute("data-valor");
+    }
+}
+
+function atualizarPagina() {
+    
+    const filtro = InputContem.value;
+    const classificacaoSalva = valorSelecionado;
+
+
+    ConsultaOp(ApiConsulta, '1', filtro, classificacaoSalva).catch(error => {
+        console.error('Erro ao consultar OP:', error);
+    });
+
+}
+
+// Chama a função de atualização a cada 15 minutos (em milissegundos)
+const intervaloDeAtualizacao = 15 * 60 * 1000; // 15 minutos
+setInterval(atualizarPagina, intervaloDeAtualizacao);
