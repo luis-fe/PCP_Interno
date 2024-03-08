50ae82ddc82335aa6a8de498f8850a902a8a8c02
luis-fe <luisengp@gmail.com>
2024-03-08 08:50:16 -0300

    convertendo em lista

--- models/outlet.py
+++ models/outlet.py
@@ -8,12 +8,11 @@ def AnaliseVendasOutlet():
     conn = ConexaoCSW.Conexao()
 
     CapaPedidos = pd.read_sql(BuscasAvancadas.CapaPedido(1,'2024-01-01','2024-09-01','145'),conn)
-    #CapaPedidos = pd.read_sql(BuscasAvancadas.TipoOP(), conn)
-   # PedidosSkus = pd.read_sql(BuscasAvancadas.pedidosNivelSKU(1,'2024-01-01','2024-09-01','145'),conn)
+    PedidosSkus = pd.read_sql(BuscasAvancadas.pedidosNivelSKU(1,'2024-01-01','2024-09-01','145'),conn)
 
-    #analise = pd.merge(CapaPedidos,PedidosSkus,on='codPedido',how='left')
+    analise = pd.merge(CapaPedidos,PedidosSkus,on='codPedido',how='left')
 
     conn.close()
 
-    return CapaPedidos
+    return analise
 
