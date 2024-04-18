4a5085b790866eaa2c4a1b0958ab067cdc738ca3
luis-fe <luisengp@gmail.com>
2024-04-18 16:42:33 -0300

    convertendo em lista

--- models/monitorFaturamento.py
+++ models/monitorFaturamento.py
@@ -262,6 +262,7 @@ def MonitorDePreFaturamento(empresa, iniVenda, finalVenda, tiponota,rotina, ip, 
     pedidos['Entregas Restantes'] = pedidos['entregas_Solicitadas'] - pedidos['entregas_enviadas']
     pedidos['Entregas Restantes'] = pedidos.apply(lambda row: 1 if row['entregas_Solicitadas'] <= row['entregas_enviadas'] else row['Entregas Restantes'], axis=1)
     pedidos['Entregas Restantes'] = pedidos['Entregas Restantes'].astype(str)
+    pedidos['Entregas Restantes'] = pedidos['Entregas Restantes'].str.replace('.0','')
     pedidos = pd.merge(pedidos, dadosConfPer, on='Entregas Restantes', how='left')
     etapa16 = controle.salvarStatus_Etapa16(rotina, ip, etapa15, ' Encontrando a categoria do produto')#Registrar etapa no controlador
 
