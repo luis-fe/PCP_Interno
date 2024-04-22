830ba3bb09810d5dd827ad5c9ac00823b2517e82
luis-fe <luisengp@gmail.com>
2024-04-22 11:05:08 -0300

    convertendo em lista

--- models/monitorFaturamento.py
+++ models/monitorFaturamento.py
@@ -669,8 +669,10 @@ def Classificacao(pedidos, parametro):
 def ExisteCalculoAberto(rotina):
         conn = ConexaoPostgreMPL.conexao2()
         consulta = pd.read_sql("""
-        select * from (  select status,(now():: time  - substring(inicio,12,5)::time) as ultimoTempo from "Reposicao".configuracoes.controle_requisicao_csw 
-        where rotina = %s and status = 'em andamento') df where  df.ultimoTempo < '00:07:00' 
+        select * from 
+        (select status,(now():: time  - substring(inicio,12,5)::time) as ultimoTempo from "Reposicao".configuracoes.controle_requisicao_csw 
+        where rotina = %s and status = 'em andamento') df 
+        where  df.ultimoTempo < '00:07:00' 
         """, conn, params=(rotina,))
 
         conn.close()
