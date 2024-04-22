da525c90fddeb3e05828e6c8f453bf4a9a67ef10
luis-fe <luisengp@gmail.com>
2024-04-22 14:51:13 -0300

    convertendo em lista

--- models/monitorFaturamento.py
+++ models/monitorFaturamento.py
@@ -819,7 +819,18 @@ def Ciclo2(pedidos1,avaliar_grupo):
 
 def APICongeladaCiclo2(empresa, iniVenda, finalVenda, tiponota,rotina, ip, datainicio,parametroClassificacao, tipoData):
     tiponota = '1,2,3,4,5,6,7,8,10,24,92,201,1012,77,27,28,172,9998,66,67,233,237'#Arrumar o Tipo de Nota 40
-    pedidos = pd.read_csv('monitor1.csv')
+    pedidos = pd.read_csv('monitor.csv')
+
+    def avaliar_grupo(df_grupo):
+        if (df_grupo['Distribuicao'] == 'SIM(Redistribuir)').all():
+            return 'True'
+        if (df_grupo['Distribuicao'] == 'SIM').all():
+            return 'True'
+        else:
+            return 'False'
+
+    pedidos = Ciclo2(pedidos,avaliar_grupo)
+
     pedidos['codPedido'] = pedidos['codPedido'].astype(str)
     pedidos['codCliente'] = pedidos['codCliente'].astype(str)
     pedidos["StatusSugestao"].fillna('-', inplace=True)
