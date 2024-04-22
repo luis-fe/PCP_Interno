5360639a10f44bcf132c351c6b90d7a6f773eafd
luis-fe <luisengp@gmail.com>
2024-04-22 15:28:51 -0300

    convertendo em lista

--- models/monitorFaturamento.py
+++ models/monitorFaturamento.py
@@ -738,7 +738,7 @@ def AbrirArquivoFast():
 def Ciclo2(pedidos1,avaliar_grupo):
     estoque = EstoqueSKU()
 
-    pedidos1 = pedidos1.drop(['EstoqueLivre','estoqueAtual','estReservPedido'],inplace=True)
+    pedidos1 = pedidos1.drop(['EstoqueLivre','estoqueAtual','estReservPedido'],inplace=True, axis=1)
     pedidos1['codProduto'].fillna(0,inplace=True)
     pedidos1['codProduto']=pedidos1['codProduto'].astype(str)
     pedidos1['codProduto'] = pedidos1['codProduto'].str.replace('.0','')
