da916cb6e3c58de38124a40b5b2c25a62ff5a05a
luis-fe <luisengp@gmail.com>
2024-05-09 09:26:54 -0300

    fazendo a restricao nas partes

--- models/CargaOPs.py
+++ models/CargaOPs.py
@@ -102,7 +102,9 @@ def OPemProcesso(empresa, AREA, filtro = '-', filtroDiferente = '', tempo = 9999
 
         requisicoes = pd.concat([requisicoes, partes], ignore_index=True)
         requisicoes['contagem'] = 1
-        requisicoes['contagem2'] = partes.groupby('numeroOP')['contagem'].transform('sum')
+        requisicoes['contagem2'] = requisicoes.groupby('numeroOP')['contagem'].transform('sum')
+
+
         # xx Nessa etapa é excluida as colunas "fase" e "numero" para dar uma limpada no dataframe, deixando mais limpo.
         requisicoes.drop(['fase','numero'], axis=1, inplace=True)
 
