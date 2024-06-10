f9d93e2199836c535e9b49fe1eb4618a6fde94e6
luis-fe <luisengp@gmail.com>
2024-06-10 15:05:18 -0300

    Editando as Apis do PCP Plano /Colecao

--- models/monitorFaturamento.py
+++ models/monitorFaturamento.py
@@ -454,12 +454,11 @@ def MonitorDePreFaturamento(empresa, iniVenda, finalVenda, tiponota,rotina, ip, 
 
     #23- Salvando os dados gerados em csv
     #retirar as seguintes colunas: StatusSugestao, situacaobloq, dias_a_adicionar, Resultado
-
+    print(pedidos)
     fp.write('monitor.parquet', pedidos)
 
     etapa25 = controle.salvarStatus_Etapa25(rotina, ip, etapa24, 'Salvando os dados gerados no postgre')#Registrar etapa no controlador
     return pedidos
-
 
 def API(empresa, iniVenda, finalVenda, tiponota,rotina, ip, datainicio,parametroClassificacao, tipoData,arrayRepres_excluir, arrayRepre_Incluir):
     tiponota = '1,2,3,4,5,6,7,8,10,24,92,201,1012,77,27,28,172,9998,66,67,233,237'#Arrumar o Tipo de Nota 40
